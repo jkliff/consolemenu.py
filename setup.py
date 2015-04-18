05f5571c9c710941dedf44699ddce8cedac848b6
john <john@bataille>
2015-04-18 23:05:01 +0200

    prepare setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='H7R python terminal menu',
+    version='0.0.1',
+    package_dir={'': 'src'},
+    py_modules=['consolemenu'],
+#    scripts=['src/consolemenu.py'],
+)
