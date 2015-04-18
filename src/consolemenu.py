19391a2fe4f4c8eaedcef3185b39c48d651749ba
john <john@bataille>
2015-04-18 23:04:52 +0200

    add documentation, cleanup

--- src/consolemenu.py
+++ src/consolemenu.py
@@ -1,3 +1,18 @@
+#!python
+"""
+Command line menu prompt.
+Produces a multi-option prompt with selection possible through keyboard UP and DOWN strokes.
+
+- The function menu_prompt returns the index of the selected option.
+- If called as a script, prints to stdout the option (useful to build interactive shells scripts from languages othar 
+than Python).
+
+Example as shell utility: 
+$ python -m consolemenu "asdf" "qwer" "foo" "bar bababa?"
+
+
+"""
+
 import sys
 import termios
 import tty
@@ -14,7 +29,7 @@ def f():
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setcbreak(sys.stdin.fileno())
-        # escape sequences are 3 bytes long, but we might get an \n, so we check the first anyways
+        # escape sequences are 3 bytes long, but we might get an \n and want to bail out immediatelly.
         ch = sys.stdin.read(1)
         if ch != '\n':
             ch += sys.stdin.read(2)
@@ -23,7 +38,7 @@ def f():
     return ch
 
 
-def print_menu(position, options):
+def __print_menu(position, options):
     sys.stdout = buf = StringIO()
 
     for i, o in enumerate(options):
@@ -34,7 +49,7 @@ def print_menu(position, options):
     return buf.getvalue()[:-1]
 
 
-erase_lines = lambda count: sys.stdout.write((CURSOR_UP_ONE + ERASE_LINE) * count)
+__erase_lines = lambda count: sys.stdout.write((CURSOR_UP_ONE + ERASE_LINE) * count)
 
 
 def menu_prompt(options):
@@ -42,7 +57,7 @@ def menu_prompt(options):
 
     while True:
 
-        buf = print_menu(position, options)
+        buf = __print_menu(position, options)
         print buf
 
         v = f()
@@ -52,15 +67,12 @@ def menu_prompt(options):
             position = min(position + 1, len(options) - 1)
         elif v[0] == '\n':
             return position
-        else:
-            print v[0], v[1], v[2]
-            raw_input()
 
-        erase_lines(len(options))
-    return 0
+        __erase_lines(len(options))
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 1:
         sys.exit(1)
+    # TODO: provide cli switch to make output be return code or stdout, as below
     print menu_prompt(sys.argv[1:])
